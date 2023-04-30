f860dabcf541b840ec15de1cb37b20136474858e
Vlad Shamrai <cocomber9@gmail.com>
2023-04-30 10:10:18 +0300

    start TelBot API

---
+++ src/main/java/com/example/tgbot/BotConfig.java
@@ -0,0 +1,20 @@
+package com.example.tgbot;
+
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@Data
+@PropertySource("application.properties")
+public class BotConfig {
+
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${bot.key}")
+    String token;
+}
---
+++ src/main/java/com/example/tgbot/controller/TelegramBot.java
@@ -0,0 +1,15 @@
+package com.example.tgbot.controller;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+
+
+}
+
